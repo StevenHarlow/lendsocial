15a02dca538fc7dcc3a7897cd0bedcea11de919f
Goss Nuzzo-Jones <gnuzzojones@zipcar.com>
2011-10-11 21:01:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LendSocial::Application.config.secret_token = 'bdeeaeabf2deea539e67e8122066df71f1da47494c2dbd613159ad2da4db08d1006c65db7d5aa3f9c47a942a36111d8bbee54aa9b86c9811dc0df98519924d65'

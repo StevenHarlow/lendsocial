12736e8f959131a909170807310a1d0c41ce5558
Goss Nuzzo-Jones <gnuzzojones@zipcar.com>
2012-01-08 22:48:04 -0500

    removed username, just use email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120105044254) do
+ActiveRecord::Schema.define(:version => 20120109034301) do
 
   create_table "businesses", :force => true do |t|
     t.string   "name"
@@ -90,7 +90,6 @@ ActiveRecord::Schema.define(:version => 20120105044254) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username"
     t.string   "email"
     t.string   "crypted_password"
     t.string   "password_salt"

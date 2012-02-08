f923f403c038cb1b8d5081611d032e55f8f1d0cd
James Bond <james.bond@lendsocial.com>
2012-02-07 16:25:00 -0800

    added has_many associations

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,5 @@
 class Business < ActiveRecord::Base
     has_many :user_businesses
     has_many :users, :through => :user_businesses
+    has_many :postings, :class_name => "Message", :as => :posted_to
 end

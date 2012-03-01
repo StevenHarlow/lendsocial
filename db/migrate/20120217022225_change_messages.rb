bf4fe34f4cf5f534bd8d7cde95df2171d5f77cef
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2012-03-01 02:41:26 +0200

    messages without limits

--- db/migrate/20120217022225_change_messages.rb
+++ db/migrate/20120217022225_change_messages.rb
@@ -4,7 +4,7 @@ class ChangeMessages < ActiveRecord::Migration
       t.remove_references :message_type
       t.references :by_business
       t.index :by_business_id
-      t.change(:text, :text, :limit => 2000)
+      t.change :text, :text
     end
   end
 
@@ -12,7 +12,7 @@ class ChangeMessages < ActiveRecord::Migration
     change_table :messages do |t|
       t.references :message_type
       t.remove_references :by_business
-      t.change(:text, :string)
+      t.change :text, :string
     end
   end
 end

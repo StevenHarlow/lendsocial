a92c339ee4be87e1ca5191e4953bf5d400ec6bec
James Bond <james.bond@lendsocial.com>
2012-04-17 18:19:34 -0700

    update & display number of lenders and amount pledged

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -11,12 +11,14 @@ class PledgesController < ApplicationController
   def create
     @loan = Loan.find(params[:loan_id])
     create_transaction
+    update_loan
     create_message
 
     respond_to do |format|
       begin
         ActiveRecord::Base.transaction do
           @transaction.save!
+          @loan.save!
           @message.save! if @message.text != ''
         end
       rescue
@@ -36,6 +38,11 @@ class PledgesController < ApplicationController
     @transaction.user_id = current_user.id
     @transaction.loan_id = @loan.id
     @transaction.business_id = @loan.business.id
+  end
+  
+  def update_loan
+    @loan.total_committed += BigDecimal.new(params[:transaction][:amount])
+    @loan.lender_count += 1
   end
 
   def create_message

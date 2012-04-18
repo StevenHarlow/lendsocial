cdabb014a0c45ce4cb76cabce1863f6ffe8658c7
James Bond <james.bond@lendsocial.com>
2012-04-17 21:36:25 -0700

    updated seed data for Rock On! School of Music

---
+++ db/fixtures/business_followings.rb
@@ -0,0 +1,6 @@
+BusinessFollowing.seed_once(:id,
+  {:id => 1, :follower_id => 10, :followed_id => 5},
+  {:id => 2, :follower_id => 6, :followed_id => 5},
+  {:id => 3, :follower_id => 9, :followed_id => 5},
+  {:id => 4, :follower_id => 7, :followed_id => 5},
+)
--- db/fixtures/user_businesses.rb
+++ db/fixtures/user_businesses.rb
@@ -1,7 +1,7 @@
 UserBusiness.seed_once(:id,
   {:id => 1, :user_id => 1, :business_id => 1, :role => 'Owner'},
   {:id => 2, :user_id => 2, :business_id => 2, :role => 'Owner'},
-  {:id => 3, :user_id => 3, :business_id => 3, :role => 'Owner'},
-  {:id => 4, :user_id => 7, :business_id => 4, :role => 'Owner'},
-  {:id => 5, :user_id => 10, :business_id => 5, :role => 'Owner'},
+  {:id => 3, :user_id => 8, :business_id => 3, :role => 'Owner'},
+  {:id => 4, :user_id => 10, :business_id => 4, :role => 'Owner'},
+  {:id => 5, :user_id => 7, :business_id => 5, :role => 'Owner'},
 )

dae9ea6ebddddb3d696bfbe4831c0f066bc14873
James Bond <james.bond@lendsocial.com>
2012-04-25 13:07:10 -0700

    moved db transaction logic for pledges into model, handle error message display

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -7,47 +7,18 @@ class PledgesController < ApplicationController
     @message = Message.new
   end
 
-
   def create
     @loan = Loan.find(params[:loan_id])
-    create_transaction
-    update_loan
-    create_message
-
+    @transaction = Transaction.new_pledge(params[:transaction][:amount], current_user, @loan)
+    @message = @loan.business.postings.build(author_id: current_user.id, text: params[:message][:text])
+    
     respond_to do |format|
-      begin
-        ActiveRecord::Base.transaction do
-          @transaction.save!
-          @loan.save!
-          @message.save! if @message.text != ''
-        end
-      rescue
-        format.html { render action: "new" }
-      else
+      if @transaction.save_pledge(@loan, @message)
         format.html { render :thanks, notice: 'Pledge was successfully created.' }
+      else
+        format.html { render action: "new" }
       end
     end
-  end
-
-  protected
-  
-  def create_transaction
-    @transaction = Transaction.new
-    @transaction.tx_type = :pledge
-    @transaction.amount = params[:transaction][:amount]
-    @transaction.user_id = current_user.id
-    @transaction.loan_id = @loan.id
-    @transaction.business_id = @loan.business.id
-  end
-  
-  def update_loan
-    @loan.total_committed += BigDecimal.new(params[:transaction][:amount])
-    @loan.lender_count += 1
-  end
-
-  def create_message
-    @message = @loan.business.postings.build(author_id: current_user.id)
-    @message.text = params[:message][:text]
   end
   
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,48 @@ class Transaction < ActiveRecord::Base
   belongs_to :loan
   belongs_to :business
   
-  validates_presence_of :amount
-  validates :amount, :numericality => { :only_integer => true, :greater_than => 0 }
+  validates_numericality_of :amount, :only_integer => true, :greater_than => 0,
+    :message => ': Please enter the dollar amount of your pledge'
+
+  def self.new_pledge(amount, user, loan)
+    Transaction.new do |t|
+      t.tx_type = :pledge
+      t.amount = amount
+      t.user_id = user.id
+      t.loan_id = loan.id
+      t.business_id = loan.business.id
+    end
+  end
+  
+  def save_pledge(loan, message = nil)
+    success_status = true
+    ActiveRecord::Base.transaction do
+      if !self.save
+        success_status = false
+      else
+        # add amount to loan only if transaction passed validation
+        loan.total_committed += amount
+        loan.lender_count += 1
+        if !loan.save
+          success_status = false
+        end
+      end
+
+      # try saving message (if there is one)
+      # regardless of whether transaction/loan save succeeded above
+      # in order to collect all validation errors in one go
+      if message != nil && message.text != ''
+        if !message.save
+          success_status = false
+        end
+      end
+          
+      # if any validation errors occurred, rollback transaction
+      if !success_status
+        raise ActiveRecord::Rollback
+      end
+    end
+    return success_status
+  end
+  
 end
